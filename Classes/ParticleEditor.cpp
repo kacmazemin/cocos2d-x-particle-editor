105defc5e75130718e43d9536e957a2a2ac2d82a
mehmeteminkacmaz <mehmetemin.kacmaz@masomo.com>
2020-08-03 22:43:13 +0300

    deleted stb_image from project

--- Classes/ParticleEditor.cpp
+++ Classes/ParticleEditor.cpp
@@ -10,9 +10,6 @@
 #include <zlib/include/zlib.h>
 #include "base/CCDirector.h"
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
 #include "2d/CCParticleSystem.h"
 #include "2d/CCParticleExamples.h"
 #include "base/base64.h"
@@ -38,37 +35,6 @@ namespace
             "GL_ONE_MINUS_DST_ALPHA",
             "GL_SRC_ALPHA_SATURATE"
     };
-}
-
-// Simple helper function to load an image into a OpenGL texture with common settings
-bool LoadTextureFromFile(const char* filename, GLuint* out_texture, int* out_width, int* out_height)
-{
-    // Load from file
-    int image_width = 0;
-    int image_height = 0;
-    unsigned char* image_data = stbi_load(filename, &image_width, &image_height, NULL, 4);
-    if (image_data == NULL)
-        return false;
-
-    // Create a OpenGL texture identifier
-    GLuint image_texture;
-    glGenTextures(1, &image_texture);
-    glBindTexture(GL_TEXTURE_2D, image_texture);
-
-    // Setup filtering parameters for display
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    // Upload pixels into texture
-    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
-    stbi_image_free(image_data);
-
-    *out_texture = image_texture;
-    *out_width = image_width;
-    *out_height = image_height;
-
-    return true;
 }
 
 std::string compressToGzip(unsigned char* input, const size_t inputSize)
